e98a1527624cbea103ecd63e9df3c0b3669f0705
OlegAdamovich <oleg.adamovich@yahoo.com>
2020-08-25 00:08:34 +0300

    Updates

---
+++ precache-manifest.c1ec4c49bbb9e5c600d8fdc33b65d3b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62132298e13592e927fe8877c02a6f1f",
+    "url": "/AdvancedCounter/index.html"
+  },
+  {
+    "revision": "16331ee7d931f795545a",
+    "url": "/AdvancedCounter/static/css/main.9f927bc7.chunk.css"
+  },
+  {
+    "revision": "74537640474cae94f01d",
+    "url": "/AdvancedCounter/static/js/2.e0aff7db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdvancedCounter/static/js/2.e0aff7db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16331ee7d931f795545a",
+    "url": "/AdvancedCounter/static/js/main.e2b16922.chunk.js"
+  },
+  {
+    "revision": "e89e4de87c4979eba10e",
+    "url": "/AdvancedCounter/static/js/runtime-main.1776b8b3.js"
+  }
+]);
